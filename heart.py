468cdbe26f7dc7474834b06de45c26284491c97a
IF_PAGIA <139111635+pemweb9@users.noreply.github.com>
2025-05-03 21:38:03 +0700

    heart.py

--- heart.py
+++ heart.py
@@ -14,11 +14,15 @@ fbs = st.number_input('fbs')
 restecg = st.number_input('restecg')
 thalach = st.number_input('thalach')
 exang = st.number_input('exang')
+oldpeak = st.number_input('oldpeak')
+slope = st.number_input('slope')
+ca = st.number_input('ca')
+thal = st.number_input('thal')
 
 predict = ''
 
 if st.button('Heart Attack'):
     predict = model.predict(
-        [[age, sex, cp, trestbps, chol, fbs, restecg, thalach, exang]]
+        [[age, sex, cp, trestbps, chol, fbs, restecg, thalach, exang, oldpeak, slope, ca, thal]]
     )
     st.write('Heart Attack: ', predict)
